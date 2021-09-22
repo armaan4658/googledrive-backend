ffb42e861e96810b71c8b5d2528d951a84874d72
armaan4658 <armaan4658@gmail.com>
2021-09-22 14:48:16 +0530

    added multiple domains

--- index.js
+++ index.js
@@ -20,8 +20,22 @@ app.get('/',(request,response)=>{
 
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = "https://google-drive-ak-front-end.netlify.app" || "http://localhost:3000" ;
-app.use(cors({credentials:true,origin:allowedOrigins}));
+// const allowedOrigins = "https://google-drive-ak-front-end.netlify.app" || "http://localhost:3000" ;
+// app.use(cors({credentials:true,origin:allowedOrigins}));
+app.use(function (req, res, next) {
+
+  var allowedDomains = ['http://localhost:3000','https://google-drive-ak-front-end.netlify.app' ];
+  var origin = req.headers.origin;
+  if(allowedDomains.indexOf(origin) > -1){
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+})
 
 app.use("/user",router);
 app.use("/otp",otpRouter);
